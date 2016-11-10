964ca8a815108748a4f954b3ab2da1a1c57545d6
YoshiyukiKato <yo.shuuuuki@gmail.com>
2016-11-10 23:54:56 +0900

    update

--- app/app.go
+++ app/app.go
@@ -1,11 +1,13 @@
 package app
 
 import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
-	"net/http"
-	"encoding/json"
 )
 
 func init() {
@@ -17,62 +19,85 @@ func init() {
 		v1.GET("/gimmick/:id", getGimmick)
 		v1.POST("/gimmick/:id", setGimmick)
 	}
-	
+
 	http.Handle("/", router)
 }
 
-type Gimmicks struct {
-	Gimmicks []Gimmick `json:gimmicks`
+//Gimmick data model.
+type gimmick struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Src         string `json:"src"`
+	Media       string `json:"media"`
+	Env         string `json:"env"`
+	Path        string `json:"path"`
+	PageAction  string `json:"pageAction"`
+	Persona     string `json:"persona"`
 }
 
-type Gimmick struct {
-  Name string	`json:"name"`
-	Description string	`json:"description"`
-	Src string	`json:"src"`
-	Media string	`json:"media"`
-	Env string	`json:"env"`
-	Path string	`json:"path"`
-	PageAction string	`json:"pageAction"`
-	Persona string	`json:"persona`
+//Gimmicks is a list of Gimmick.
+type gimmicks struct {
+	List []gimmick `json:"list"`
 }
 
-func getGimmicks(ctx *gin.Context){
-	gaeCtx = appengine.newContext(ctx.Request)
-	
-	var gimmicks Gimmicks
-	keys, err := datastore.NewQuery("Gimmick").GetAll(gaeCtx, &gimmicks.Gimmicks)
-	ctx.JSON(200, json.Marshal(gimmicks));
+func getGimmicks(ctx *gin.Context) {
+	gaeCtx := appengine.NewContext(ctx.Request)
+	var gimmicks gimmicks
+	keys, err := datastore.NewQuery("Gimmick").GetAll(gaeCtx, &gimmicks.List)
+
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	} else {
+		ctx.JSON(200, json.Marshal(gimmicks))
+	}
 }
 
-func newGimmick(ctx *gin.Context){
-	var gimmick Gimmick
-	ctx.BindJson(gimmick)
-
-	gaeCtx = appengine.newContext(gin.Context.Request)
-	key := datastore.NewKey(gaeCtx, "Gimmick", "", 0, nil)
-	datastore.Put(gaeCtx, key, &gimmick);
+func newGimmick(ctx *gin.Context) {
+	var gimmick gimmick
+	ctx.BindJSON(&gimmick)
+	gaeCtx := appengine.NewContext(ctx.Request)
+	newkey := datastore.NewKey(gaeCtx, "Gimmick", "", 0, nil)
+	key, err := datastore.Put(gaeCtx, newkey, &gimmick)
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	} else {
+		ctx.JSON(200, gin.H{"message": "complete!"})
+	}
 }
 
-func getGimmick(ctx *gin.Context){
-	id := ctx.params("id");
-	var gimmick Gimmick
-	gaeCtx = appengine.newContext(gin.Context.Request)
-	datastore.Get(gaeCtx, id, &gimmick)
+func getGimmick(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	}
 
-	ctx.JSON(200, json.Marshal(gimmick));
+	var gimmick gimmick
+	gaeCtx := appengine.NewContext(ctx.Request)
+	key := datastore.NewKey(gaeCtx, "Gimmick", "", id, nil)
+	_key, err := datastore.Get(gaeCtx, key, &gimmick)
+
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	} else {
+		ctx.JSON(200, json.Marshal(gimmick))
+	}
 }
 
-func setGimmick(ctx *gin.Context){
-	id := ctx.params("id");
-	var gimmick Gimmick
-	ctx.BindJson(gimmick)
+func setGimmick(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	}
 
-	gaeCtx = appengine.newContext(gin.Context.Request)
-	datastore.Put(gaeCtx, id, &gimmick)
-}
+	var gimmick gimmick
+	ctx.BindJSON(&gimmick)
+	gaeCtx := appengine.NewContext(ctx.Request)
+	key := datastore.NewKey(gaeCtx, "Gimmick", "", id, nil)
+	_key, err := datastore.Put(gaeCtx, key, &gimmick)
 
-func test(ctx *gin.Context){
-	ctx.JSON(200, gin.H{
-		"message": "test!",
-	})
+	if err != nil {
+		ctx.JSON(500, gin.H{"message": err})
+	} else {
+		ctx.JSON(200, gin.H{"message": "complete!"})
+	}
 }
