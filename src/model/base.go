9430646e5f4f22b8ad9eccdf1296491314982989
YoshiyukiKato <yo.shuuuuki@gmail.com>
2016-11-14 12:04:54 +0900

    add base model

--- src/model/base.go
+++ src/model/base.go
@@ -1,27 +1,26 @@
-package model
+package base
 
 import (
   "net/http"
   "errors"
-  "strconv"
-
-  "github.com/gin-gonic/gin"
   "google.golang.org/appengine"
   "google.golang.org/appengine/datastore"
 )
-
-//Paramをmap[string]interface{}型にして指定できるようにする（とかもあり
-//&Param{ "type" : "string", "notNull" : true, "defaultTo" : "hogehoge" }
 
 //Model is interface for datastore
 type Model struct{
   Kind string
   Context appengine.Context
-  Instance Instance
+  Instance interface{}
 }
 
-type Instance interface{
+type Instance struct{
   Key datastore.Key
+}
+
+func (m Model) Init(r *http.Request) Model{
+  m.Context = appengine.NewContext(r)
+  return m
 }
 
 func (m Model) New(params map[string] interface{}) Instance, Error{
@@ -33,7 +32,7 @@ func (m Model) New(params map[string] interface{}) Instance, Error{
     // フィールドの取得
     field := schema.Field(i)
     param = params[field.Name]
-    if param != nil && reflect.TypeOf(param) == field.Type {
+    if param != nil && reflect.TypeOf(param) == field.Type{
       instance.Field(i).Set(param)
     }
   }
@@ -42,11 +41,15 @@ func (m Model) New(params map[string] interface{}) Instance, Error{
 }
 
 func (m Model) Save(instance Instance) Instance, Error{
-  if instance.Key == nil {
-    newkey := datastore.NewKey(Model.Context, Model.Kind, "", 0, nil)
+  if m.Context == nil {
+    return nil, errors.New("Context is not initialized")
   }
 
-  key, err := datastore.Put(Model.Context, newkey, &instance)
+  if instance.Key == nil {
+    newkey := datastore.NewKey(m.Context, m.Kind, "", 0, nil)
+  }
+
+  key, err := datastore.Put(m.Context, newkey, &instance)
   if key != nil {
     instance.Key = key
   }
@@ -54,42 +57,25 @@ func (m Model) Save(instance Instance) Instance, Error{
   return instance, err
 }
 
-func (m Model) Init(r *http.Request) Model{
-  m.Context = appengine.NewContext(r)
-  return m
-}
-
-func (m Model) All() []Instance, Error {
+func (m Model) Find(key datastore.Key) interface{}, Error{
   if m.Context == nil {
     return nil, errors.New("Context is not initialized")
-  }
-  return m.Where(nil)
-}
-
-func (m Model) Find(strkey) interface{}, Error {
-  if m.Context == nil {
-    return nil, errors.New("Context is not initialized")
-  }
-  
-  key, keyerr := datastore.DecodeKey(strkey)
-  if keyerr != nil {
-    return nil, keyerr
   }
   
   schema := reflect.TypeOf(m.Instance)
   instance := reflect.New(schema).Elem()
-  if geterr := datastore.Get(m.Context, key, instance); geterr == nil {
+  if geterr := datastore.Get(m.Context, key, instance); geterr == nil{
     instance.Key = key
   }
   
   return instance, geterr
 }
 
-func (m Model) FindBy(paramName, paramValue) interface{}, Error{
-  if m.Context == nil {
-    return nil, errors.New("Context is not initialized")
-  }
+func (m Model) All() []Instance, Error {
+  return m.Where(nil)
+}
 
+func (m Model) FindBy(paramName string, paramValue interface{}) interface{}, Error{ 
   terms := map["string"]interface{}{
     paramName + " =" : paramValue
   }
@@ -98,8 +84,11 @@ func (m Model) FindBy(paramName, paramValue) interface{}, Error{
 }
 
 func (m Model) Where(terms map[string]interface{}) []interface{}, Error{
+  if m.Context == nil {
+    return nil, errors.New("Context is not initialized")
+  }
+
   query := datastore.NewQuery(m.Kind)
-  
   if terms != nil{
     for filterStr, value := range terms{
       query.Filter(filterStr, value)
--- src/model/model.go
+++ src/model/model.go
@@ -1,11 +1,28 @@
 package model
 
+import (
+	"./base"
+)
+
 //Gimmicks is a list of Gimmick.
 type Gimmicks struct {
 	Items []Gimmick `json:"items"`
 }
 
 //Gimmick data model.
+type Gimmick struct{
+	base.Model
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Src         string `json:"src"`
+	Media       string `json:"media"`
+	Env         string `json:"env"`
+	Path        string `json:"path"`
+	PageAction  string `json:"pageAction"`
+	Persona     string `json:"persona"`
+}
+
+/*
 type Gimmick struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -16,3 +33,4 @@ type Gimmick struct {
 	PageAction  string `json:"pageAction"`
 	Persona     string `json:"persona"`
 }
+*/
